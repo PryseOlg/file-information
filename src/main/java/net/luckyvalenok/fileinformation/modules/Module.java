22fb92e869b91a025984000fb06f152b662ab7b7
PryseOlg <oleg.kuts.02@mail.ru>
2023-03-24 12:42:37 +0500

    Complete and Added in GitHub

---
+++ src/main/java/net/luckyvalenok/fileinformation/modules/Module.java
@@ -0,0 +1,19 @@
+package net.luckyvalenok.fileinformation.modules;
+
+import java.io.File;
+
+public abstract class Module {
+    public abstract boolean isSuitableExtension(File file);
+    
+    public abstract String getDescription();
+    
+    public abstract void executeCommand(String command, File file);
+    
+    protected final String getExtension(File file) {
+        String fileName = file.getName();
+        if (fileName.lastIndexOf(".") != -1) {
+            return fileName.substring(fileName.lastIndexOf(".") + 1);
+        }
+        return null;
+    }
+}
---
+++ src/test/java/net/luckyvalenok/fileinformation/FileInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.luckyvalenok.fileinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileInformationApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
